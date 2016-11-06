d6eadcf311e6e21746182d1480e97b350dda2b3e
thomas.friedel <thomas.friedel@berlin>
2016-11-06 22:03:38 +0100

    removed reference to other files from setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import os
 from setuptools import setup
-from setuptools.command.install import install
-from lightify import __version__
-
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
 
 setup(
     name='lightify',
@@ -16,7 +7,7 @@ setup(
     include_package_data=True,
     license='BSD License',
     description='A library to work with OSRAM lightify.',
-    long_description=README,
+    long_description='A library to work with OSRAM lightify. Threadsafe.',
     url='https://github.com/tfriedel/python-lightify',
     author='Thomas Friedel',
     author_email='thomas.friedel@gmail.com',
