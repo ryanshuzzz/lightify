1bb1db0e7bd5b14304d7bb267e2398cd5160df46
thomas.friedel <thomas.friedel@berlin>
2017-03-12 20:31:19 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lightify',
-    version='1.0.4',
+    version='1.0.5',
     packages=['lightify'],
     include_package_data=True,
     license='BSD License',
